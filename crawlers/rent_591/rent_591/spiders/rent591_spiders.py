06dadb257bfbcac6b28999717f917c18037f88d5
Mark Chang <ak0922747401@gmail.com>
2019-02-19 23:08:52 +0800

    init spider

---
+++ crawlers/rent_591/rent_591/spiders/rent591_spiders.py
@@ -0,0 +1,15 @@
+"""Rent591 spiders."""
+
+import scrapy
+
+
+class Rent591Spider(scrapy.Spider):
+    """Rent591 spiders class."""
+
+    name = "rent591"
+
+    def start_requests(self):
+        pass
+
+    def parse(self, response):
+        pass
