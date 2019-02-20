ad97bff2e410557571755fa643b42b3c49b21f59
Mark Chang <ak0922747401@gmail.com>
2019-02-20 21:05:22 +0800

    update start_requests logic add city cookie mapping

--- crawlers/rent_591/rent_591/spiders/rent591_spiders.py
+++ crawlers/rent_591/rent_591/spiders/rent591_spiders.py
@@ -10,13 +10,21 @@ class Rent591Spider(scrapy.Spider):
     name = "rent591"
 
     def start_requests(self):
-        pages = 0
-        url = f'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&shType=list&firstRow={pages*30}' # noqa
-        yield scrapy.Request(
-            url=url,
-            cookies={'urlJumpIp': 1},
-            callback=self.parse_house_id
-        )
+        page_num = 10
+        cookies_mapping = {
+            '台北市': 1,
+            '新北市': 3,
+        }
+        for k in cookies_mapping.keys():
+            for page in range(page_num):
+                url = f'https://rent.591.com.tw/home/search/rsList?is_new_list=1&region={cookies_mapping[k]}&type=1&kind=0&searchtype=1&shType=list&firstRow={page*30}' # noqa
+                request = scrapy.Request(
+                    url=url,
+                    cookies={'urlJumpIp': cookies_mapping[k]},
+                    callback=self.parse_house_id
+                )
+                request.meta['city'] = k
+                yield request
 
     def parse_house_id(self, response):
         res = json.loads(response.body.decode("utf-8"))
@@ -24,8 +32,16 @@ class Rent591Spider(scrapy.Spider):
         logger.debug(house_ids)
         for house_id in house_ids:
             detail_url = f'https://rent.591.com.tw/rent-detail-{house_id}.html'
-            yield response.follow(detail_url, self.parse_detail)
+            request = scrapy.Request(
+                url=detail_url,
+                callback=self.parse_detail
+            )
+            request.meta['city'] = response.meta['city']
+            request.meta['house_id'] = house_id
+            yield request
 
     def parse_detail(self, response):
+        logger.debug(f"city:{response.meta['city']}")
+        logger.debug(f"id:{response.meta['house_id']}")
         phone_number = response.css('.dialPhoneNum::attr(data-value)').get()
         logger.debug(phone_number)
