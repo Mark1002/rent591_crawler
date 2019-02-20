f1ffda4cc1c031a8f263aae542212f6bafd2dffb
Mark Chang <ak0922747401@gmail.com>
2019-02-20 17:34:05 +0800

    init spider logic

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## this is my crawlers pratices.
+
+reference
+1. https://ithelp.ithome.com.tw/articles/10191506
--- crawlers/rent_591/rent_591/spiders/rent591_spiders.py
+++ crawlers/rent_591/rent_591/spiders/rent591_spiders.py
@@ -1,6 +1,7 @@
 """Rent591 spiders."""
-
+import json
 import scrapy
+from ajilog import logger
 
 
 class Rent591Spider(scrapy.Spider):
@@ -9,7 +10,22 @@ class Rent591Spider(scrapy.Spider):
     name = "rent591"
 
     def start_requests(self):
-        pass
+        pages = 0
+        url = f'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1&shType=list&firstRow={pages*30}' # noqa
+        yield scrapy.Request(
+            url=url,
+            cookies={'urlJumpIp': 1},
+            callback=self.parse_house_id
+        )
 
-    def parse(self, response):
-        pass
+    def parse_house_id(self, response):
+        res = json.loads(response.body.decode("utf-8"))
+        house_ids = [el['houseid'] for el in res['data']['data']]
+        logger.debug(house_ids)
+        for house_id in house_ids:
+            detail_url = f'https://rent.591.com.tw/rent-detail-{house_id}.html'
+            yield response.follow(detail_url, self.parse_detail)
+
+    def parse_detail(self, response):
+        phone_number = response.css('.dialPhoneNum::attr(data-value)').get()
+        logger.debug(phone_number)
