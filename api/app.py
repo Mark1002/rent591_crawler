c923a36fb1c15aba706dbfe3120b9286c6adb9af
Mark Chang <ak0922747401@gmail.com>
2019-02-23 20:57:58 +0800

    init rent API endpoint

--- api/app.py
+++ api/app.py
@@ -1,1 +1,20 @@
 """API endpoint."""
+from flask import (
+    Flask,
+    jsonify,
+    request,
+)
+
+from services import get_rent_house_list
+
+app = Flask(__name__)
+
+
+@app.route("/rent")
+def rent():
+    """Rent API endpoint."""
+    params = request.args.to_dict()
+    rent_list = get_rent_house_list(**params)
+    return jsonify({
+        'data': rent_list
+    })
