17b64b8eaf26e037f848bc4014555e05af92eaa0
Mark Chang <ak0922747401@gmail.com>
2019-02-23 10:00:07 +0800

    add mongoDB query logic

--- api/services.py
+++ api/services.py
@@ -21,4 +21,19 @@ def get_rent_house_list(**kwargs) -> List[dict]:
     """
     client = pymongo.MongoClient(settings.MONGO_URI)
     db_name = settings.MONGO_DATABASE
-    pass
+
+    cursor = client[db_name]['rents'].find(kwargs)
+    rent_list = [{
+        'house_id': rent['house_id'],
+        'house_type': rent['house_type'],
+        'house_recent': rent['house_recent'],
+        'city': rent['city'],
+        'renter': rent['renter'],
+        'renter_type': rent['renter_type'],
+        'renter_sex': rent['renter_sex'],
+        'phone_number': rent['phone_number'],
+        'sex_limit': rent['sex_limit']
+    } for rent in cursor]
+    # close db connection
+    client.close()
+    return rent_list
